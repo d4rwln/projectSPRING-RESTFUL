f494c7d471dc97a49bed626426a4777c86460bd1
d4rwln <darwinop@gmail.com>
2022-04-24 16:51:00 -0300

    Resource User test

--- src/main/java/com/dwsolutions/projectSPRING1/resources/UserResource.java
+++ src/main/java/com/dwsolutions/projectSPRING1/resources/UserResource.java
@@ -1,5 +1,19 @@
 package com.dwsolutions.projectSPRING1.resources;
 
-public class UserResource {
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.dwsolutions.projectSPRING1.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1,"Jose", "d@mail.com","99887766","123123");
+		return ResponseEntity.ok().body(u);
+	}
 }
