6662f9251aed36e1663b98d8949e50c1e3b1baad
d4rwln <darwinop@gmail.com>
2022-04-23 22:39:31 -0300

    Start Project

---
+++ src/main/java/com/dwsolutions/projectSPRING1/ProjectSpring1Application.java
@@ -0,0 +1,13 @@
+package com.dwsolutions.projectSPRING1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSpring1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSpring1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/dwsolutions/projectSPRING1/ProjectSpring1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dwsolutions.projectSPRING1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSpring1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
